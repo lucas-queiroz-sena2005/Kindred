278154538fbcf68ec7797e9d3979ea895a8438c0
lucas-queiroz-sena2005 <lucas.queiroz.sena2005@gmail.com>
2025-10-16 00:14:26 -0300

    Feed centered.

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -5,11 +5,18 @@ import Navbar from './Navbar';
 
 function Layout(): React.ReactElement {
     return (
-        <div className="bg-neutral-100 text-neutral-900 min-h-screen font-sans">
-            <Topbar />
-            <Navbar />
-            <main className="p-8">
-                <Outlet />
+        <div className="flex flex-col items-center bg-neutral-100 text-neutral-900 min-h-screen font-sans">
+            {/* Full-width bars */}
+            <div className="w-full">
+                <Topbar />
+                <Navbar />
+            </div>
+
+            {/* Centered page wrapper */}
+            <main className="block w-screen flex justify-center p-8">
+                <div className="w-[60%] max-w-6xl min-w-[300px]">
+                    <Outlet />
+                </div>
             </main>
         </div>
     );
