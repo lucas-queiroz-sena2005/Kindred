c897c01cf8d86b17950c9e59fe04914251aba2fd
lucas-queiroz-sena2005 <lucas.queiroz.sena2005@gmail.com>
2025-10-18 20:07:14 -0300

    Authentication middleware in route.

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,17 +1,15 @@
 import { Router } from 'express';
 import authRouter from './auth.js';
+import userRouter from './me.js';
+import { isAuthenticated } from '../middleware/isAuthenticated.js';
 
 const router = Router();
 
-/**
- * @route   GET /api
- * @desc    API health check
- * @access  Public
- */
 router.get('/', (req, res) => {
   res.json({ message: 'API is alive and running!' });
 });
 
 router.use('/auth', authRouter);
+router.use('/', isAuthenticated, userRouter);
 
 export default router;
