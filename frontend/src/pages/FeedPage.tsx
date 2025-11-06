b46bc5e389ad479a72cf153ddcbe7d57e7e7b7f1
lucas-queiroz-sena2005 <lucas.queiroz.sena2005@gmail.com>
2025-11-06 19:27:28 -0300

    feat: infinite scroll on feed.

--- frontend/src/pages/FeedPage.tsx
+++ frontend/src/pages/FeedPage.tsx
@@ -1,38 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { api } from "../api";
-import type { TierListSummary } from "../types/tierlist";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useOnScrollToBottom } from "../hooks/useOnScrollToBottom";
 import { TemplateCard } from "../components/TemplateCard";
 
 function FeedPage(): React.ReactElement {
-  const [data, setData] = useState<TierListSummary[] | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
+  const PAGE_LIMIT = 10;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    isError,
+    error,
+  } = useInfiniteQuery({
+    queryKey: ["feedTemplates"],
+    queryFn: ({ pageParam }) =>
+      api.tierlists.getList({
+        sortBy: "createdAt",
+        filter: "all",
+        limit: PAGE_LIMIT,
+        offset: pageParam,
+      }),      
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      const totalFetched = allPages.flat().length;
+      return lastPage.length < PAGE_LIMIT ? undefined : totalFetched;
+    },
+  });
 
-  useEffect(() => {
-    api.tierlists
-      .getList()
-      .then((data) => {
-        setData(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setIsLoading(false);
-      });
-  }, []);
+  useOnScrollToBottom(fetchNextPage, hasNextPage, isFetchingNextPage);
+
+  const templates = data?.pages.flat() ?? [];
+
+  const showNoMoreTemplates =
+    !isLoading && !isFetchingNextPage && !hasNextPage && templates.length > 0;
 
   return (
     <>
       <h1 className="text-3xl font-bold mb-4">Feed</h1>
+
       {isLoading && <p>Loading tierlists...</p>}
-      {error && <p className="text-red-500">Error: {error.message}</p>}
-      {data && (
+      {isError && <p className="text-red-500">Error: {error.message}</p>}
+
+      {templates.length > 0 && (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {data.map((template) => (
+          {templates.map((template) => (
             <TemplateCard key={template.id} {...template} />
           ))}
         </div>
       )}
+
+      {isFetchingNextPage && <div>Loading more...</div>}
+      {showNoMoreTemplates && <div>No more templates to show.</div>}
     </>
   );
 }
